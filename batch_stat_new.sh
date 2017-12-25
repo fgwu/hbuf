63c7d63d4f71f916bd42a9ebfd2ef44f787fb2d9
Fenggang Wu <wuxx0835@umn.edu>
2017-12-25 02:23:52 -0600

    output filename new customizable

--- batch_stat_new.sh
+++ batch_stat_new.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 logdir=./logfiles_all
-:> stat.log
+outfile=stat.log
+:> $outfile
 
 for f in $(ls tracefile_ssd/*csv); do
     trace=$(echo $f | grep -Eo [[:alnum:]]+_[0-9])
-    echo -n $trace " " | tee -a stat.log
+    echo -n $trace " " | tee -a $outfile
     for p in setasso rand sliding singlelog media; do
 	#	echo -n trace $trace policy $p
 	:> ${logdir}/${trace}_${p}.log
 	./sim $f $p > ${logdir}/${trace}_${p}.log
 	overhead=$(cat ${logdir}/${trace}_${p}.log | grep overhead | awk '{print $2}')
-	echo -n $overhead "," | tee -a stat.log
+	echo -n $overhead "," | tee -a $outfile
     done
-    echo | tee -a stat.log
+    echo | tee -a $outfile
 done
 
